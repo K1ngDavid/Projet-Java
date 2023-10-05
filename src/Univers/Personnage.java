c5ae303f7714ae7c750a8d78f3ef7af99277d85d
k1ngdavid <david.roufe@gmail.com>
2023-10-05 16:40:51 +0200

    Création de la classe Personnage

--- src/Univers/Personnage.java
+++ src/Univers/Personnage.java
@@ -1,4 +1,21 @@
 package Univers;
 
+//TODO: #1 Créer la classe 'Personnage'
+
 public class Personnage {
+    private String pseudo;
+    private String image;
+
+    public Personnage(String pseudo){
+        this.pseudo = pseudo;
+    }
+
+    public Personnage(String pseudo, String image){
+        this.pseudo = pseudo;
+        this.image = image;
+    }
+
+    public String getPseudo(){
+        return this.pseudo;
+    }
 }
