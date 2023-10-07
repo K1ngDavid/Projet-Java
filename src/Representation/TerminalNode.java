7d5e0acdbdb0ff1857e963faafaaf35c4dc81043
k1ngdavid <david.roufe@gmail.com>
2023-10-08 00:35:19 +0200

    Création de la classe 'TerminalNode'

---
+++ src/Representation/TerminalNode.java
@@ -0,0 +1,14 @@
+package Representation;
+
+//TODO : Création de 'TerminalNode.java'
+
+public class TerminalNode extends Node {
+
+    public TerminalNode(String description) {
+        super(description);
+        //TODO Auto-generated constructor stub
+    }
+
+
+    
+}
