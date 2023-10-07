ff81d1c4187737e825e0b142c87e56811161c660
k1ngdavid <david.roufe@gmail.com>
2023-10-07 23:56:43 +0200

    Création de la classe mère 'Node.java' Fixes #3

---
+++ src/Representation/Node.java
@@ -0,0 +1,23 @@
+package Representation;
+
+//TODO: #3 Création de la classe mère 'Node.java'
+
+public abstract class Node {
+		private int id;
+		private String description;
+		private static int  nbNodes = 0;
+
+		public Node(String description){
+			this.id = nbNodes;
+			this.description = description;
+			nbNodes++;
+		}
+
+		public void display(){
+			System.out.println(this.description);
+		}
+
+		public Node chooseNext(){
+			return this;
+		}
+}
