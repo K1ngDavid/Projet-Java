c6fa6d10dc82228e887fe3a85dba080bc6312a23
k1ngdavid <david.roufe@gmail.com>
2023-12-18 12:44:55 +0100

    Ajout de la JavaDoc

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Game"},{"l":"Game.Tile"},{"l":"Representation"},{"l":"Tools"},{"l":"Univers"},{"l":"Univers.demon"},{"l":"Univers.empereur"}];updateSearchResults();
